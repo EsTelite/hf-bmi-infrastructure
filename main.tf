9dcb7f3c8b9f5beb31f76103c4620b74b5f1abef
riski <riskicahyadi@gmail.com>
2021-09-19 18:49:29 +0700

    block all public to s3

--- main.tf
+++ main.tf
@@ -50,4 +50,6 @@ resource "aws_s3_bucket_public_access_block" "block-public-acl" {
   bucket = aws_s3_bucket.lambda-bucket.id
   block_public_acls   = true
   block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
 }
