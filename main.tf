dbf25c12f75ab84eb5e04cf3fca6bd1c30c55914
riski <riskicahyadi@gmail.com>
2021-09-19 18:43:26 +0700

    disable public acl

--- main.tf
+++ main.tf
@@ -37,7 +37,6 @@ locals {
 resource "aws_s3_bucket" "lambda-bucket" {
   bucket = format("%s-%s-lambda-bucket",var.environment,var.project)
   policy = templatefile("${path.cwd}/template/s3-restrict-only-https.json", { "bucket-name" = local.lambda-bucket })
-  block_public_acls   = true
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
@@ -45,4 +44,9 @@ resource "aws_s3_bucket" "lambda-bucket" {
       }
     }
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "block-public-acl" {
+  bucket = aws_s3_bucket.lambda-bucket.id
+  block_public_acls   = true
 }
